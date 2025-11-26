950c4024292ad019a19d547e792df87e05af8a58
Amir Farhan Nasrullah <hi.amirfarhannasrullah@gmail.com>
2025-11-26 18:40:07 +0700

    update logo

--- resources/views/layouts/master_nav.blade.php
+++ resources/views/layouts/master_nav.blade.php
@@ -6,7 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'KAMCUP')</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" href="{{ asset('assets/img/logo4.png') }}" type="image/png">
+    <link rel="icon" href="{{ asset('assets/img/lgo.jpeg') }}" type="image/png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet">
