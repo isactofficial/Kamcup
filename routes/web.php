89cab2982743906c4ae66ece23ddc996610b3686
MyCannedFood <sagarawaluya@gmail.com>
2026-03-06 09:58:55 +0700

    fix quick login

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\FrontController;
+use App\Http\Controllers\Auth\GoogleController;
 use App\Http\Controllers\Auth\PasswordController;
 use App\Http\Controllers\Auth\ResetPasswordController;
 use App\Http\Controllers\GalleryController;
